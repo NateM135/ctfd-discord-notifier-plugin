9198aeb7aad834b682742477afd660889b17c832
NateM135 <melwaninate@gmail.com>
2023-07-23 18:52:14 -0700

    get num solvers

--- src.py
+++ src.py
@@ -1,7 +1,9 @@
 from functools import wraps
-from flask import Flask, request, g
-from CTFd.utils.user import get_current_team, get_current_user
+from flask import request
+
+from CTFd.models import Challenges, Solves
 from CTFd.utils import config as ctfd_config
+from CTFd.utils.user import get_current_team, get_current_user
 
 def load(app):
     print("PLUGIN IS WORKING")
@@ -13,7 +15,6 @@ def load(app):
     def challenge_attempt_decorator(f):
         @wraps(f)
         def wrapper(*args, **kwargs):
-            TEAMS_MODE = ctfd_config.is_teams_mode()
             result = f(*args, **kwargs)
 
             # Retrieve all needed information
@@ -24,14 +25,20 @@ def load(app):
             if TEAMS_MODE:
                 print("Teams Mode")
                 print(f"Team Name: {get_current_team().name}")
-
+            else:
+                print("Not Teams Mode")
             print(f"Current User: {get_current_user().name}")
     
             challenge_id = current_request.get('challenge_id')
             correctness = current_response.get("data").get("status")
-
             print(f"Challenge ID: {challenge_id}")
             print(f"Submission Correctness: {correctness}")
+
+            challenge = Challenges.query.filter_by(id=challenge_id).first()
+            solvers = Solves.query.filter_by(challenge_id=challenge.id)
+            num_solves = solvers.count()
+            print(f"Solve Count: {num_solves}")
+
             return result
         return wrapper
 
